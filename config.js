5c197c92a972fa00919378389f8cd387cae6e333
Bobi Cothran <roughwatergypsy@gmail.com>
2024-05-30 16:11:55 -0500

    .js and .json add

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-5b2e15ac-3963-4c89-bc4c-634db1f2271c.containerhub.tripleten-services.com/"
 }
